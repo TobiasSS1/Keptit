a1ebef194eff5bd8b5d23074cb0c35ba442b293f
rolf3d <rolf0585@edu.easj.dk>
2017-04-24 12:57:01 +0200

    Design for Greenkeeper system og Admin

--- KeptIT/View/Admin.xaml.cs
+++ KeptIT/View/Admin.xaml.cs
@@ -13,18 +13,24 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace KeptIT
+namespace KeptIT.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Admin : Page
     {
-        public MainPage()
+        public Admin()
         {
             this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
     }
 }
